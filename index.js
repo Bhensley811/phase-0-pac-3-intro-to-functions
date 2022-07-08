216f962c8c8024aedcf41e62e24ea473ccfd8f9c
Brian <Bhensley811@gmail.com>
2022-07-08 14:56:00 -0400

    commit tree 049d7e7a1ed789da034033eafe9aed92423f64d3

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Follow along with the examples here
+function sayHello() {
+    console.log("Hello!");
+  }
+
+sayHello(); 
+
+
+
+function fruits(num1, num2) {
+    return num1 + num2;
+}
+
+function veggies(num3, num4) {
+    return num3 + num4;
+}
+
+
+// Inpuyt Apples first then oranges 
+const numOfFruits = fruits(3, 4);
+const fruitsToday = ` Fruits: You've had ${numOfFruits} Today! `
+
+// input Broccoli then Spinach
+const numOfVeggies = veggies(2, 4);
+const veggiesToday = ` Veggies: You've had ${numOfVeggies} Today! `
+
+console.log(fruitsToday)
+console.log(veggiesToday)
+
+    
